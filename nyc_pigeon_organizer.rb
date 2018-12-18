cfda9eaefdae10908e1d1bd68bf39ac0f3bf2ada
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-18 20:47:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ def get_colors(data)
   hash = Hash.new([])
   
   data[:color].each do |color, arr|
-    arr.each do [name]
+    arr.each do |name|
       hash[name] << color
     end
   end
@@ -35,7 +35,7 @@ def get_gender(data)
   hash = Hash.new([])
   
   data[:gender].each do |gender, arr|
-    arr.each do [name]
+    arr.each do |name|
       hash[name] << gender
     end
   end
