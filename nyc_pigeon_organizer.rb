ee284b65a43ba22f4c056b4c18c175cd9e854a53
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-18 20:08:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  birds = {}
+  birds = Hash.new({})
+  colors = get_colors(data)
+  gender = get_gender(data)
+  lives = get_lives(data)
   
-  data[:color].each do |color, arr|
-    arr.each do |name|
-      birds[name][:color] 
+  colors.each do |name, color|
+    birds[name][:color] = color
+  end
+  
+  gender.each do |name, gender|
+    birds[name][:gender] = gender
+  end
+  
+  lives.each do |name, location|
+    birds[name][:lives]
   
 end
 
@@ -33,5 +43,13 @@ def get_gender(data)
 end
 
 def get_lives(data)
+  hash = Hash.new([])
   
+  data[:lives].each do |location, arr|
+    arr.each do |name|
+      hash[name] << location
+    end
+  end
+  
+  hash
 end
